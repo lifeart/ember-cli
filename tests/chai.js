54d296745fd6f55abae8b81ff7f245ebffe1ba94
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-01-14 18:51:12 +0300

    remove commented code

--- tests/chai.js
+++ tests/chai.js
@@ -9,16 +9,6 @@ chai.use(chaiFiles);
 chai.use(chaiAsPromised);
 chai.use(chaiJestSnapshot);
 
-// // eslint-disable-next-line mocha/no-top-level-hooks
-// before(function () {
-//   chaiJestSnapshot.resetSnapshotRegistry();
-// });
-
-// // eslint-disable-next-line mocha/no-top-level-hooks
-// beforeEach(function () {
-//   chaiJestSnapshot.configureUsingMochaContext(this);
-// });
-
 module.exports = chai;
 module.exports.file = chaiFiles.file;
 module.exports.dir = chaiFiles.dir;
